31f1f055cc8e5a068dffa632195fa3bfc0b5f5fb
ianmict <ianm.tablizo@gmail.com>
2024-10-14 20:47:40 -0600

    Update script.js

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-  let response = await fetch("https://5luuamwfd6uoyxhoyj2yuyi2yi0ogxrg.lambda-url.us-east-1.on.aws/");
+  let response = await fetch("https://9hn8n5ie91.execute-api.us-east-1.amazonaws.com/Deploy/visitors");
   let data = await response.json();
   counter.innerHTML = ` Views: ${data}`;
 }
